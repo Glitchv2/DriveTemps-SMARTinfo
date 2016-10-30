7e790aef1712802e3feb4fc1ecde01b725262e05
Glitchv2 <Glitchv2@users.noreply.github.com>
2016-10-29 22:48:52 -0500

    Update V2.sh

--- V2.sh
+++ V2.sh
@@ -14,7 +14,7 @@ else
         read -p " Would you like to continue?" yn
         case $yn in
           [Yy]* ) continue;;
-          [Nn]* ) echo "Script halted, quitting."
+          [Nn]* ) echo "Ending my life!"
         esac
         exit 126 && echo $?
       done
@@ -32,21 +32,21 @@ fi
 
 if [ ls /dev/ | grep -c '\bda[0-9]\b' ] -gt 0 ] ; then
   drivecount="$(ls /dev/ | grep -c '\bda[0-9]\b')"
+  continue
 elif [ ls /dev/ | grep -c '\bada[0-9]\b' ] -gt 0 ] ; then
   drivecount="$(ls /dev/ | grep -c '\bada[0-9]\b')"
+  continue
 else
   echo "Unable to count drives!
         while true; do
         read -p " Would you like to continue?" yn
         case $yn in
           [Yy]* ) continue;;
-          [Nn]* ) echo "Script halted, quitting."
+          [Nn]* ) echo "Ending my life!"
         esac
         exit 126 && echo $?
       done
 fi
-
-
 
 #Catches if script was unable to count drives
 if [ "$drivecount" -gt 0 ] ; then
@@ -57,7 +57,7 @@ if [ "$drivecount" -gt 0 ] ; then
         read -p " Would you like to continue?" yn
         case $yn in
           [Yy]* ) continue;;
-          [Nn]* ) echo "Script halted, quitting."
+          [Nn]* ) echo "Ending my life!"
         esac
         exit 126 && echo $?
       done
@@ -72,9 +72,12 @@ logdate=`date "+%m-%d-%Y"`
 #Sets the time for the log file contents
 logtime=`date "+%m-%d-%Y  %H:%M:%S"`
 
-#Clears the screen from all printouts
 clear
 
 echo "Processor Core Count: " $corecount
-echo "Number of 'da' Drives: " $drivecount
+echo "Count of da/ada Drives: " $drivecount
+echo "System Load 1 minute: " uptime | awk '{print $8}'
+echo "System Load 5 minute: " uptime | awk '{print $9}'
+echo "System Load 10 minute: " uptime | awk '{print $10}'
+
 exit 0
